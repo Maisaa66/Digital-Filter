7d24d4146ec9141285ce7a60fc322bfe0fc400f4
sheren390 <61457923+sheren390@users.noreply.github.com>
2021-06-18 02:01:44 +0200

    add filter graph,  clear(zeros, poles) buttons

--- app.py
+++ app.py
@@ -24,35 +24,40 @@ unit.circle(x=[0], y=[0], color="green",radius=1,alpha=0.1)
 show(unit)
 
 #plot frequency response with two graphs(magnitude & phase)
-freqGraph= figure (x_range=(0,6.28), y_range=(-10,10), tools=[],
-title='Frequency response',plot_width=600, plot_height=350)
+freqGraph= figure (x_range=(0,6.28), y_range=(-10,10),toolbar_location="right",
+title='Frequency response',plot_width=560, plot_height=350)
 freqGraph.line(x_co,y_co, legend_label="Mag",line_color="red")
 freqGraph.line(x_co,y_co, legend_label="Phase", color="black" )
 show(freqGraph)
 
+#plot filter frequency response with two graphs(magnitude & phase)
+filterGraph= figure (x_range=(0,6.28), y_range=(-10,10),toolbar_location="right",
+title='All Pass Filter',plot_width=560, plot_height=350)
+filterGraph.line(x_co,y_co, legend_label="Mag",line_color="red")
+filterGraph.line(x_co,y_co, legend_label="Phase", color="black" )
+
 ######################################################################
+
 #contains the columns you just referenced for the x_pole and y_pole
 source_1 = ColumnDataSource(data=dict(x_of_poles=[], y_of_poles=[]))
-
+#drow poles as asterisk
+renderer = unit.star(x="x_of_poles", y="y_of_poles", source=source_1, color='red', size=10)
 #table shows (x,y) for each pole 
-renderer = unit.circle(x="x_of_poles", y="y_of_poles", source=source_1, color='green', size=9)
 columns_1 = [TableColumn(field="x_of_poles", title="x_of_poles"),
-           TableColumn(field="y_of_poles", title="y_of_poles")
-           ]
+           TableColumn(field="y_of_poles", title="y_of_poles")]
 table = DataTable(source=source_1, columns=columns_1, editable=True, height=200)
 
 
 #contains the columns you just referenced for the x_zero and y_zero 
 source_2 = ColumnDataSource(data=dict(x_of_zeros=[], y_of_zeros=[]))
-
+#drow zeros as circle
+renderer_2 = unit.scatter(x='x_of_zeros', y='y_of_zeros', source=source_2, color='green', size=10)
 #table shows (x,y) for each zero
-renderer_2 = unit.asterisk(x='x_of_zeros', y='y_of_zeros', source=source_2, color='red', size=10)
 columns_2 = [TableColumn(field="x_of_zeros", title="x_of_zeros"),
-           TableColumn(field="y_of_zeros", title="y_of_zeros")
-           ]
+           TableColumn(field="y_of_zeros", title="y_of_zeros")]
 table_2 = DataTable(source=source_2, columns=columns_2, editable=True, height=200)
 
-###############################################
+#####################################################################
 
 new_source_1= ColumnDataSource({
     'h':[], 'w':[]
@@ -116,32 +121,50 @@ def clear_all():
     source_1.data['y_of_poles'].clear()
     new_data={'x_of_poles':source_1.data['x_of_poles'],'y_of_poles':source_1.data['y_of_poles'],}
     source_1.data=new_data
-    ########################################################################
+
     source_2.data['x_of_zeros'].clear()
     source_2.data['y_of_zeros'].clear()
     new_data_2={'x_of_zeros':source_2.data['x_of_zeros'],'y_of_zeros':source_2.data['y_of_zeros'],}
     source_2.data=new_data_2
-    print('deleted')
 
+
+def clear_zeros():
+    source_2.data['x_of_zeros'].clear()
+    source_2.data['y_of_zeros'].clear()
+    new_data_2={'x_of_zeros':source_2.data['x_of_zeros'],'y_of_zeros':source_2.data['y_of_zeros'],}
+    source_2.data=new_data_2
+
+def clear_poles():
+    source_1.data['x_of_poles'].clear()
+    source_1.data['y_of_poles'].clear()
+    new_data={'x_of_poles':source_1.data['x_of_poles'],'y_of_poles':source_1.data['y_of_poles'],}
+    source_1.data=new_data
+
+
+#buttons
 toggle = Toggle(label="Conjugate", button_type="success", width=90)
-toggle.js_on_click(CustomJS(code="""
-    console.log('toggle: active=' + this.active, this.toString())
-"""))
+toggle.js_on_click(CustomJS(code="""console.log('toggle: active=' + this.active, this.toString())"""))
 filterMenu = Select(options=['Choose Filter','Filter 1', 'Filter 2', 'Filter 3'],value='None' ,title='Filters')
-Clear_button=Button(label="Clear All" ,button_type="success",width=70)
-Clear_button.on_click(clear_all)
+# filterMenu.on_change('value', 'function of filters') 
+ClearAll_button=Button(label="Clear All" ,button_type="success",width=70)
+ClearP_button=Button(label="Clear Poles" ,button_type="success",width=70)
+ClearZ_button=Button(label="Clear Zeros" ,button_type="success",width=70)
+ClearAll_button.on_click(clear_all)
+ClearP_button.on_click(clear_poles)
+ClearZ_button.on_click(clear_zeros)
 ######################################################################
 
 draw_tool = PointDrawTool(renderers=[renderer], empty_value='red')
-draw_tool_2 = PointDrawTool(renderers=[renderer_2], empty_value='blue')
+draw_tool_2 = PointDrawTool(renderers=[renderer_2], empty_value='green')
 
 
 unit.add_tools(draw_tool,draw_tool_2)
 unit.toolbar.active_tap = draw_tool
+unit.toolbar.logo= None
 freqGraph.toolbar.logo = None
-freqGraph.toolbar_location = None
-plot=Row(unit, freqGraph)
+filterGraph.toolbar.logo = None
+plot=Row(unit, freqGraph,filterGraph)
 plot2=Row(table,table_2)
 menu=Row(filterMenu)
-buttons=Row(Clear_button, toggle)
+buttons=Row(toggle, ClearP_button, ClearZ_button,ClearAll_button)
 curdoc().add_root(column(plot , buttons, menu, plot2))
